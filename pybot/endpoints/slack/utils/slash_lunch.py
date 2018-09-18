f4ed5349636a0f6881e29d2c9e0195868353e817
wimo7083 <wimo7083@colorado.edu>
2018-09-18 12:47:48 -0600

    fix issue with float representation of strings

--- pybot/endpoints/slack/utils/slash_lunch.py
+++ pybot/endpoints/slack/utils/slash_lunch.py
@@ -2,8 +2,17 @@ from random import randint
 
 from zipcodes import is_valid
 
+MAX_LUNCH_RANGE = '30'
+DEFAULT_LUNCH_RANGE = '20'
+MIN_LUNCH_RANGE = '1'
+
 
 def random_zip():
+    '''
+    Because what doesn't matter is close food but good food
+    :return: zip_code
+    :rtype: str
+    '''
     random_zip = 0
     while not is_valid(str(random_zip)):
         range_start = 10 ** (4)
@@ -14,7 +23,16 @@ def random_zip():
 
 
 def within_lunch_range(input_number):
-    return int(input_number) <= 30
+    return int(input_number) <= int(DEFAULT_LUNCH_RANGE)
+
+
+def fix_param(input: str) -> str:
+    try:
+        val =  str(max(int(float(input)), int(MIN_LUNCH_RANGE)))
+    except ValueError:
+        val = DEFAULT_LUNCH_RANGE
+
+    return val
 
 
 def two_params(first_param, second_param):
@@ -24,20 +42,21 @@ def two_params(first_param, second_param):
         return {'location': random_zip(), 'range': '20'}
 
 
+# TODO: add test cases for various inputs
 def split_params(param_text):
     if not param_text:  # no params, default random zip code, 20 miles
         return {'location': random_zip(), 'range': '20'}
 
     params = param_text.split()
 
-    if len(params) == 2:  # two values
-        return two_params(params[0], params[1])
+    if len(params) == 2:
+        return two_params(fix_param(params[0]), fix_param(params[1]))
 
-    if len(params) == 1 and is_valid(params[0]):  # one value
-        return {'location': params[0], 'range': '20'}
+    if len(params) == 1 and is_valid(fix_param(params[0])):
+        return {'location': params[0], 'range': DEFAULT_LUNCH_RANGE}
 
     else:
-        return {'location': random_zip(), 'range': '20'}
+        return {'location': random_zip(), 'range': DEFAULT_LUNCH_RANGE}
 
 
 def get_random_lunch(lunch_response):
@@ -49,3 +68,9 @@ def get_random_lunch(lunch_response):
 
 def build_response_text(loc_dict):
     return f'The Wheel of Lunch has selected {loc_dict["name"]} at {" ".join(loc_dict["location"]["display_address"])}'
+
+
+if __name__ == '__main__':
+    assert(fix_param('0.5')=='1')
+    assert(fix_param('12')=='12')
+    assert(fix_param('abc')=='20')
