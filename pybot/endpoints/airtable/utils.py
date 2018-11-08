c90b8586de80b2fbc8973c9f7af739b1eaf271a6
Allen Anthes <abanthes@gmail.com>
2018-11-08 13:08:37 -0600

    cleanup airtable endpoint

--- pybot/endpoints/airtable/utils.py
+++ pybot/endpoints/airtable/utils.py
@@ -5,58 +5,63 @@ from sirbot import SirBot
 from slack import ROOT_URL, methods
 from slack.events import Message
 from slack.exceptions import SlackAPIError
-
-from pybot.endpoints.airtable.messages import (claim_mentee_attachment,
-                                               mentor_request_text)
+from slack.io.aiohttp import SlackAPI
+from pybot.plugins.airtable.api import AirtableAPI
+from .message_templates.messages import claim_mentee_attachment, mentor_request_text
 
 MENTORS_CHANNEL = os.environ.get("MENTORS_CHANNEL") or "G1DRT62UC"
 
 
-async def _get_requested_mentor(requested_mentor: Optional[str], app: SirBot) -> Optional[str]:
+async def _get_requested_mentor(requested_mentor: Optional[str], slack: SlackAPI,
+                                airtable: AirtableAPI) -> Optional[str]:
     try:
         if not requested_mentor:
             return None
-        mentor = await app.plugins['airtable'].api.get_mentor_from_record_id(requested_mentor)
+        mentor = await airtable.get_mentor_from_record_id(requested_mentor)
         email = mentor['Email']
-        slack_user_id = await _slack_user_id_from_email(email, app)
+        slack_user_id = await _slack_user_id_from_email(email, slack)
         return f" Requested mentor: <@{slack_user_id}>"
     except SlackAPIError:
         return None
 
 
-async def _slack_user_id_from_email(email: str, app: SirBot, fallback: Optional[str] = None) -> str:
-    response = Message()
-    response['email'] = email
+async def _slack_user_id_from_email(email: str, slack: SlackAPI, fallback: Optional[str] = None) -> str:
     try:
-        response = await app.plugins['slack'].api.query(url=ROOT_URL + 'users.lookupByEmail', data=response)
+        response = await slack.query(url=ROOT_URL + 'users.lookupByEmail', data={'email': email})
         return response['user']['id']
     except SlackAPIError:
         # TODO: something better here
         return fallback or 'Slack User'
 
 
-async def _get_matching_skillset_mentors(skillsets: str, app: SirBot) -> List[str]:
+async def _get_matching_skillset_mentors(skillsets: str, slack: SlackAPI, airtable: AirtableAPI) -> List[str]:
     if not skillsets:
         return ['No skillset Given']
-    mentors = await app.plugins['airtable'].api.find_mentors_with_matching_skillsets(skillsets)
-    mentor_ids = [await _slack_user_id_from_email(mentor['Email'], app, fallback=mentor['Slack Name']) for mentor in
+    mentors = await airtable.find_mentors_with_matching_skillsets(skillsets)
+    mentor_ids = [await _slack_user_id_from_email(mentor['Email'], slack, fallback=mentor['Slack Name']) for mentor in
                   mentors]
     return [f'<@{mentor}>' for mentor in mentor_ids]
 
 
 def _create_messages(mentors: List[str], request: dict, requested_mentor_message: str, service_translation: str,
-                     slack_id: str) -> Tuple[Message, Message, Message]:
-    first_message = Message()
-    first_message['text'] = mentor_request_text(slack_id, service_translation, request.get('skillsets', None),
-                                                requested_mentor_message)
-    first_message['attachments'] = claim_mentee_attachment(request['record'])
-    first_message['channel'] = MENTORS_CHANNEL
-    details_message = Message()
-    details_message['text'] = f"Additional details: {request.get('details', 'None Given')}"
-    details_message['channel'] = MENTORS_CHANNEL
-    matching_mentors_message = Message()
-    matching_mentors_message['text'] = "Mentors matching all or some of the requested skillsets: " + ' '.join(mentors)
-    matching_mentors_message['channel'] = MENTORS_CHANNEL
+                     slack_id: str) -> Tuple[dict, dict, dict]:
+    first_message = {
+        'text': mentor_request_text(slack_id, service_translation, request.get('skillsets', None),
+                                    requested_mentor_message),
+        'attachments': claim_mentee_attachment(request['record']),
+        'channel': MENTORS_CHANNEL
+    }
+
+    details_message = {
+        'text': f"Additional details: {request.get('details', 'None Given')}",
+        'channel': MENTORS_CHANNEL
+    }
+
+    matching_mentors_message = {
+        'text': "Mentors matching all or some of the requested skillsets: " + ' '.join(mentors),
+        'channel': MENTORS_CHANNEL
+    }
+
     return first_message, details_message, matching_mentors_message
 
 
