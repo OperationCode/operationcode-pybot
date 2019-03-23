e61334909d2db90a69a4dc81a11bcb7aecfe02bf
Allen Anthes <abanthes@gmail.com>
2019-03-23 10:59:27 -0500

    Style cleanup and change @here message

--- pybot/endpoints/slack/messages.py
+++ pybot/endpoints/slack/messages.py
@@ -5,6 +5,7 @@ from slack.events import Message
 from slack import methods
 
 from .message_templates.tech import TechTerms
+from .utils import BOT_URL
 
 logger = logging.getLogger(__name__)
 
@@ -15,15 +16,15 @@ def create_endpoints(plugin):
     plugin.on_message(".*\!tech", tech_tips)
     plugin.on_message(".*\<\!here\>", here_bad)
     plugin.on_message(".*\<\!channel\>", here_bad)
-    plugin.on_message(".*@here", here_bad)
-    plugin.on_message(".*@channel", here_bad)
     plugin.on_message(".*\!pybot", advertise_pybot)
 
 
 def not_bot_message(event: Message):
-    return ('message' not in event or
-            'subtype' not in event['message'] or
-            event['message']['subtype'] != 'bot_message')
+    return (
+            'message' not in event
+            or 'subtype' not in event['message']
+            or event['message']['subtype'] != 'bot_message'
+    )
 
 
 def not_bot_delete(event: Message):
@@ -31,57 +32,58 @@ def not_bot_delete(event: Message):
 
 
 async def advertise_pybot(event: Message, app: SirBot):
-    BOT_URL = 'https://github.com/OperationCode/operationcode-pybot'
-    response = {'channel': event['channel'],
-                'text': f'OC-Community-Bot is a community led project'
-                f'\n <{BOT_URL}|source> '}
+    response = dict(
+        channel=event['channel'],
+        text=f'OC-Community-Bot is a community led project\n <{BOT_URL}|source> '
+    )
+
     await app.plugins["slack"].api.query(methods.CHAT_POST_MESSAGE, data=response)
 
 
-async def here_bad(event: Message, app: SirBot):
+async def here_bad(event: Message, app: SirBot) -> None:
     if 'channel_type' in event and event['channel_type'] != 'im':
-        response = {'channel': event['channel'],
-                    'text': f'<@{event["user"]}> - you are a very bad person for using that command'}
-        await app.plugins["slack"].api.query(methods.CHAT_POST_MESSAGE, data=response)
+        user = event.get('user')
+        user_id = f'<@{user}>' if user else 'Hey you'
+        await app.plugins["slack"].api.query(methods.CHAT_POST_MESSAGE, data=dict(
+            channel=event['channel'],
+            text=f'{user_id} - this had better be important!'
+        ))
 
 
 async def tech_tips(event: Message, app: SirBot):
     if not_bot_message(event):
-        logger.info(
-            f'tech logging: {event}')
+        logger.info(f'tech tips logging: {event}')
         try:
-            tech_terms: dict = await TechTerms(event['channel'], event['user'],
-                                               event.get('text'), app).grab_values()
-
+            tech_terms = await TechTerms(
+                event['channel'], event['user'],
+                event.get('text'), app
+            ).grab_values()
             await app.plugins["slack"].api.query(methods.CHAT_POST_MESSAGE, tech_terms['message'])
-        except Exception as E:
-            logger.exception(E)
+
+        except Exception:
+            logger.debug(f'Exception thrown while logging message_changed {event}')
 
 
 async def message_changed(event: Message, app: SirBot):
     """
     Logs all message edits not made by a bot.
     """
-    # need to check for bot_delete as deletes with replies that result in a "tombstone" also send as edits
-    if not_bot_message(event) and not_bot_delete(event):
-        try:
+    try:
+        # need to check for bot_delete as deletes with replies that
+        # result in a "tombstone" also send as edits
+        if not_bot_message(event) and not_bot_delete(event):
             logger.info(
                 f'CHANGE_LOGGING: edited: {event["ts"]} for user: {event["previous_message"]["user"]}\n{event}')
-
-        except Exception as E:
-            logger.exception(E)
-            logger.debug(event)
+    except ValueError as e:
+        logger.debug(f'Exception thrown while logging message_changed. Event: {event} || Error: {e}')
 
 
 async def message_deleted(event: Message, app: SirBot):
     """
     Logs all message deletions not made by a bot.
     """
-    if not_bot_delete(event):
-        try:
-            logger.info(
-                f'CHANGE_LOGGING: deleted: {event["ts"]}\nEvent: {event}')
-
-        except Exception as E:
-            logger.exception(E)
-            logger.debug(event)
+    try:
+        if not_bot_delete(event):
+            logger.info(f'CHANGE_LOGGING: deleted: {event["ts"]}\nEvent: {event}')
+    except ValueError as e:
+        logger.debug(f'Exception thrown while logging message_deleted. Event: {event} || Error: {e}')
