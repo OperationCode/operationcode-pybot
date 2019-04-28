aaddafa0f44290ab468a0952e5ad575356c495b2
Allen Anthes <abanthes@gmail.com>
2019-04-28 15:43:16 -0500

    Cleanup scripts and add encrypted creds

--- bin/run.sh
+++ bin/run.sh
@@ -9,39 +9,40 @@ fi
 # Don't process pull requests
 # $TRAVIS_PULL_REQUEST will be the PR number or "false" if not a PR
 if [[ -n "$TRAVIS_PULL_REQUEST" ]] && [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "Skipping deploy because it's a pull request"
-  exit 0
+    echo "Skipping deploy because it's a pull request"
+    exit 0
 fi
 
 # Only process branches listed in DEPLOY_BRANCHES
-BRANCHES_TO_DEPLOY=($DEPLOY_BRANCHES)
+BRANCHES_TO_DEPLOY=( ${DEPLOY_BRANCHES} )
 if [[ ! " ${BRANCHES_TO_DEPLOY[@]} " =~ " ${TRAVIS_BRANCH} " ]]; then
-  # whatever you want to do when arr contains value
-  echo "Skipping deploy, not a branch to be deployed"
-  exit 0
+    # whatever you want to do when arr contains value
+    echo "Skipping deploy, not a branch to be deployed"
+    exit 0
 fi
 
 pip install awscli -q
 
-if [ $? = 0 ]; then
-  AWSBIN=$(which aws)
-  AWSPATH=$(dirname $AWSBIN)
-  export PATH=$PATH:$AWSPATH
+if [[ $? = 0 ]]; then
+    AWSBIN=$(which aws)
+    AWSPATH=$(dirname ${AWSBIN})
+    export PATH=$PATH:${AWSPATH}
 
-  # Get absolute path of dir where run.sh is located
-  SOURCE="${BASH_SOURCE[0]}"
-  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  done
-  export SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    # Get absolute path of dir where run.sh is located
+    SOURCE="${BASH_SOURCE[0]}"
+    while [[ -h "$SOURCE" ]]; do
+        # resolve $SOURCE until the file is no longer a symlink
+        DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    export SCRIPTDIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-  bash ${SCRIPTDIR}/build &&
-  bash ${SCRIPTDIR}/publish &&
-  bash ${SCRIPTDIR}/deploy
+    bash ${SCRIPTDIR}/build.sh &&
+    bash ${SCRIPTDIR}/publish.sh &&
+    bash ${SCRIPTDIR}/deploy.sh
 
 else
-  echo "Failed to install AWS CLI"
-  exit 1
+    echo "Failed to install AWS CLI"
+    exit 1
 fi
