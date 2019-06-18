50d6db2bef5729a58396af7a4a15b3c1bb80014b
Allen Anthes <abanthes@gmail.com>
2019-06-18 09:27:57 -0500

    Use circleCI config instead of scripts

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-# import util functions
-source "${SCRIPTDIR}/../lib/util.sh"
-
-echo "Building Docker image..."
-runCommand "docker build -t $IMAGE_NAME -f docker/Dockerfile ." || exit $?
